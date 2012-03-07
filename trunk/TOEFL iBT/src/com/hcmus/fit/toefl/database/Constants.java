5ed6bb739adcc46d3d056458acf1cff3b01d2c53
vinhphuoc1990@gmail.com <vinhphuoc1990@gmail.com@6045b587-b097-f478-ebfc-f4b33786a5b5>
2012-03-07 07:49:26 +0000

    123

---
+++ trunk/TOEFL iBT/src/com/hcmus/fit/toefl/database/Constants.java
@@ -0,0 +1,10 @@
+package com.hcmus.fit.toefl.database;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Constants {
+	public static final String DATABASE_NAME = "Toefl iBT";
+	public static final int DATABASE_VERSION = 1;
+}
