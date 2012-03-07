10907ae5badaab75862719a49e039bd7300a5ca0
vinhphuoc1990 <vinhphuoc1990@gmail.com>
2012-03-07 08:09:20 +0000

    splash

--- TOEFL iBT/src/com/hcmus/fit/toefl/MainActivity.java
+++ TOEFL iBT/src/com/hcmus/fit/toefl/MainActivity.java
@@ -1,15 +1,19 @@
 package com.hcmus.fit.toefl;
 
-import com.hcmus.fit.toefl.database.MyDB;
-
 import android.app.Activity;
-import android.os.Bundle;
-import android.view.MotionEvent;
-import android.widget.LinearLayout;
-import android.animation.*;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+	private static final int STOPSPLASH = 0;
+	// time in milliseconds
+	private static final long SPLASHTIME = 3000;
+	private ImageView splash;
 
 	/** Called when the activity is first created. */
 
@@ -17,6 +21,33 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		splash = (ImageView) findViewById(R.id.imgMainCover);
+
+		Message msg = new Message();
+		msg.what = STOPSPLASH;
+		try {
+			splashHandler.sendMessageDelayed(msg, SPLASHTIME);
+		} catch (Exception e) {
+		}
+	}
+
+	private Handler splashHandler = new Handler() {
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case STOPSPLASH:
+				// remove SplashScreen from view
+				splash.setVisibility(View.GONE);
+				startMain();
+			}
+			super.handleMessage(msg);
+		}
+	};
+
+	private void startMain() {
+		startActivity(new Intent(this, MainScreen.class));
+		overridePendingTransition(R.anim.scaleup, R.anim.scaledown);
+		finish();
 	}
 
 	// @Override
