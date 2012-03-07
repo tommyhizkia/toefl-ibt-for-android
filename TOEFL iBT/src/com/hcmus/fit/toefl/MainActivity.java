7d51736950fd11457d6f01767c92cc1e403669cd
vinhphuoc1990 <vinhphuoc1990@gmail.com>
2012-03-07 07:49:26 +0000

    123

--- TOEFL iBT/src/com/hcmus/fit/toefl/MainActivity.java
+++ TOEFL iBT/src/com/hcmus/fit/toefl/MainActivity.java
@@ -1,5 +1,7 @@
 package com.hcmus.fit.toefl;
 
+import com.hcmus.fit.toefl.database.MyDB;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -8,21 +10,21 @@ import android.animation.*;
 import android.content.Intent;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-    }
-    
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-    	// TODO Auto-generated method stub
-    	Intent newIntent = new Intent(this, MainScreen.class);
-    	startActivity(newIntent);
-    	overridePendingTransition(R.anim.scaleup, R.anim.scaledown);
-    	return super.onTouchEvent(event);
-    }
+
+	/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	// @Override
+	// public boolean onTouchEvent(MotionEvent event) {
+	// // TODO Auto-generated method stub
+	// Intent newIntent = new Intent(this, MainScreen.class);
+	// startActivity(newIntent);
+	// overridePendingTransition(R.anim.scaleup, R.anim.scaledown);
+	// return super.onTouchEvent(event);
+	// }
 }
